3d957de8efdc9e11799ffd2f6a2e8760f5f815a1
zldeng <hitdzl@gmail.com>
2013-03-19 09:40:40 +0800

    update training command file and README file

--- pos.train.sh
+++ pos.train.sh
@@ -1,3 +1,4 @@
+#train model with a single thread, we set compression ratio to be 0.2
 nohup java -Xms15G -Xmx30G -cp OnlineLabel.jar ir.hit.edu.ltp.parser.OnlinePos \
 -train -trainFile ./data/pos/train.conll06.pos \
 -dicFile ./data/pos/conll06.pos.dic \
@@ -5,3 +6,13 @@ nohup java -Xms15G -Xmx30G -cp OnlineLabel.jar ir.hit.edu.ltp.parser.OnlinePos \
 -iterator 40  \
 -devFile ./data/pos/dev.conll06.pos.gold \
 -compress 0.2 &
+
+#train model with Multi-thread, we set compression ratio to be 0.2 and set thread number to be 10
+nohup java -Xms15G -Xmx30G -cp OnlineLabel.jar ir.hit.edu.ltp.parser.OnlinePos \
+-train -trainFile ./data/pos/train.conll06.pos \
+-dicFile ./data/pos/conll06.pos.dic \
+-model  ./model/conll06.model \
+-iterator 40  \
+-devFile ./data/pos/dev.conll06.pos.gold \
+-compress 0.2 
+-thread 10 &
--- seg.train.sh
+++ seg.train.sh
@@ -1,3 +1,4 @@
+#train with a single thread,we set compression ratio to be 0.2
 nohup java -Xms15G -Xmx30G -cp OnlineLabel-1.3.jar ir.hit.edu.ltp.parser.OnlineSeg \
 -train  -trainFile ./data/seg/pku.train  \
 -model ./model/pku.seg.model \
@@ -5,3 +6,13 @@ nohup java -Xms15G -Xmx30G -cp OnlineLabel-1.3.jar ir.hit.edu.ltp.parser.OnlineS
 -dicFile ./data/seg/pku.seg.dic \
 -iterator 40 \
 -compress 0.2 &
+
+#train with Multi-threads,we set compression ratio to be 0.2 and set thread number to be 10 
+nohup java -Xms15G -Xmx30G -cp OnlineLabel-1.3.jar ir.hit.edu.ltp.parser.OnlineSeg \
+-train  -trainFile ./data/seg/pku.train  \
+-model ./model/pku.seg.model \
+-devFile ./data/seg/pku.test.gold  \
+-dicFile ./data/seg/pku.seg.dic \
+-iterator 40 \
+-compress 0.2 
+-thread 10 &
